593feea86d97ab4183a49c443b3634ee0a413106
moajo <mimirosiasd@gmail.com>
2015-08-04 09:18:15 +0900

    第5回終了

--- ShootingGame/ShootingGame/Assets/Scripts/Bullet.cs
+++ ShootingGame/ShootingGame/Assets/Scripts/Bullet.cs
@@ -5,10 +5,12 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
 	public float speed=10;
+	public float lifeTime=5;
 	// Use this for initialization
 	void Start () 
 	{
 		GetComponent<Rigidbody2D> ().velocity = transform.up.normalized * speed;
+		Destroy (gameObject, lifeTime);
 	
 	}
 	
